0ccd1559c9718dee2b2e5c0dc72f334bdcf4f565
Chae Cramb <chaecramb@gmail.com>
2024-01-28 20:19:54 +0000

    feat: implements highestNumber

--- challenges/exercise7-optional.js
+++ challenges/exercise7-optional.js
@@ -15,7 +15,7 @@ export function shoppingList(list, newItem) {
 
 export function highestNumber(numbers) {
   if (!numbers) throw new Error("numbers are required");
-  // Your solution using the spread operator here!
+  return Math.max(...numbers);
 }
 
 export function splitThatString(string) {
