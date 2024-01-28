561b7a5a148de7c5de5fba8ee0701a5826035f44
Chae Cramb <chaecramb@gmail.com>
2024-01-28 20:04:24 +0000

    feat: implements createRange

--- challenges/exercise6-optional.js
+++ challenges/exercise6-optional.js
@@ -27,10 +27,10 @@ export const sumDigits = (n) => {
 export const createRange = (start, end, step) => {
   if (start === undefined) throw new Error("start is required");
   if (end === undefined) throw new Error("end is required");
-  if (step === undefined)
-    console.log(
-      "FYI: Optional step parameter not provided. Remove this check once you've handled the optional step!"
-    );
+  if (step === undefined) step = 1;
+  return Array.from({ length: (end - start) / step + 1 }, (_, i) => {
+    return start + i * step;
+  });
 };
 
 /**
